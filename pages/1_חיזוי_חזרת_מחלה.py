2dde53fbd8a1f4232782d0584374a264d3b1477a
HANIBE555 <hanibe555@gmail.com>
2025-07-24 11:58:36 +0300

    Update 1_חיזוי_חזרת_מחלה.py

--- pages/1_חיזוי_חזרת_מחלה.py
+++ pages/1_חיזוי_חזרת_מחלה.py
@@ -4,6 +4,24 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import accuracy_score
 import numpy as np
+import base64
+
+def set_background(image_file):
+    with open(image_file, "rb") as f:
+        data = base64.b64encode(f.read()).decode()
+    css = f"""
+    <style>
+    .stApp {{
+        background-image: url("data:image/png;base64,{data}");
+        background-size: cover;
+        background-position: top left;
+        background-repeat: no-repeat;
+    }}
+    </style>
+    """
+    st.markdown(css, unsafe_allow_html=True)
+
+set_background("images/ING2.png")
 
 # עיצוב RTL
 st.markdown("""
